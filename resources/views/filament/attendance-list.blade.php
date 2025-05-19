e1122b0ba5d7b262d8980523927aeffdaa269f93
Juan Rosas <juan.rosasr01@gmail.com>
2025-05-18 20:34:43 -0400

    Refactor attendance list view for improved readability and display full name with DNI

--- resources/views/filament/attendance-list.blade.php
+++ resources/views/filament/attendance-list.blade.php
@@ -11,7 +11,7 @@
         </div>
     </x-slot>
 
-    @if($beneficiaries->count() > 0)
+    @if ($beneficiaries->count() > 0)
         <x-filament-tables::container class="mt-4">
             <x-filament-tables::table>
                 <x-slot name="header">
@@ -24,34 +24,23 @@
                     <x-filament-tables::header-cell width="20%">
                         DNI
                     </x-filament-tables::header-cell>
-                    @if(!$readOnly)
+                    @if (!$readOnly)
                         <x-filament-tables::header-cell width="10%">
                             Acciones
                         </x-filament-tables::header-cell>
                     @endif
                 </x-slot>
 
-                @foreach($beneficiaries as $beneficiary)
+                @foreach ($beneficiaries as $beneficiary)
                     <x-filament-tables::row>
-                        <x-filament-tables::cell class="text-sm font-medium">
-                            {{ $loop->iteration }}
-                        </x-filament-tables::cell>
                         <x-filament-tables::cell class="text-sm">
-                            {{ $beneficiary->full_name }}
+                            {{ $beneficiary->full_name }}({{ $beneficiary->dni }})
                         </x-filament-tables::cell>
-                        <x-filament-tables::cell class="text-sm">
-                            {{ $beneficiary->dni }}
-                        </x-filament-tables::cell>
-                        
-                        @if(!$readOnly)
+
+                        @if (!$readOnly)
                             <x-filament-tables::cell class="text-right">
-                                <x-filament::button 
-                                    wire:click="removeAttendance({{ $beneficiary->id }})"
-                                    size="sm"
-                                    color="danger"
-                                    icon="heroicon-o-trash"
-                                    tooltip="Eliminar asistencia"
-                                />
+                                <x-filament::button wire:click="removeAttendance({{ $beneficiary->id }})" size="sm"
+                                    color="danger" icon="heroicon-o-trash" tooltip="Eliminar asistencia" />
                             </x-filament-tables::cell>
                         @endif
                     </x-filament-tables::row>
@@ -60,7 +49,7 @@
         </x-filament-tables::container>
     @else
         <x-filament::card>
-            <div class="text-center p-4">
+            <div class="text-center">
                 <x-heroicon-o-user-group class="mx-auto h-12 w-12 text-gray-400" />
                 <h3 class="mt-2 text-sm font-medium text-gray-900 dark:text-gray-100">
                     {{ $readOnly ? 'Ningún beneficiario asistió' : 'No hay beneficiarios registrados aún' }}
